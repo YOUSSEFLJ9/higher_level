f3a80ba0197634ee7e4234395562bee1aa042b24
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-06-09 01:52:20 +0100

    task done

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import sys
     tot = 0
     for i in range(len(sys.argv) - 1):
-         tot += int(sys.argv[i + 1])
+        tot += int(sys.argv[i + 1])
     print("{}".format(tot))
