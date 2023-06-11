dbf79cd0f301de66a4bf77b6f760e74b4b3deb06
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-06-11 18:27:21 +0100

    hot fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,21 +4,16 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
     if len(sys.argv) == 4:
-        if sys.argv[2] == "+":
-                print("{} + {} = {} ".format(int(sys.argv[1]) ,int(sys.argv[3]), add(int (sys.argv[1]),int (sys.argv[3]))))
-                sys.exit(0)
-        elif sys.argv[2] == "-":
-                print("{} - {} = {} ".format(int(sys.argv[1]) ,int(sys.argv[3]), sub(int (sys.argv[1]),int (sys.argv[3]))))
-                sys.exit(0)
-        elif sys.argv[2] == "*":
-                print("{} * {} = {} ".format(int(sys.argv[1]) ,int(sys.argv[3]), mul(int (sys.argv[1]),int (sys.argv[3]))))
-                sys.exit(0)
-        elif sys.argv[2] == "/":
-                print("{} / {} = {} ".format(int(sys.argv[1]) ,int(sys.argv[3]), div(int (sys.argv[1]),int (sys.argv[3]))))
-                sys.exit(0)
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        operator = {"+": add, "-": sub, "*": mul, "/":div}
+        if sys.argv[2] in list(operator.keys()):
+            print("{} {} {} = {}".format(a, sys.argv[2], b, operator[sys.argv[2]](a, b)))
+            sys.exit(0)
         else:
-            print ("Unknown operator. Available operators: +, -, * and /")
+            print("Unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
+
     else:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
