15d49e13edd271a930021b5d15ee7272ae661310
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-06-28 00:03:07 +0100

    task done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """represent class square"""
+    def __init__(self, size):
+        """initializ a square
+        Args:
+        size(int): The size of the new square
+        """
+        self.__size = size
